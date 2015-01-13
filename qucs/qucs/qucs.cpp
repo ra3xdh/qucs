b97b4a4cffbad0164e432e31aa27a3b057e19434
Vadim Kuznetzov <ra3xdh@gmail.com>
2015-01-13 11:16:20 +0300

    Added stub class for Xyce execution

--- qucs/qucs/qucs.cpp
+++ qucs/qucs/qucs.cpp
@@ -3067,7 +3067,7 @@ void QucsApp::slotSimulateWithSpice()
 {
     Schematic *sch = (Schematic*)DocumentTab->currentPage();
 
-    NgspiceSimDialog *SimDlg = new NgspiceSimDialog(sch);
+    ExternSimDialog *SimDlg = new ExternSimDialog(sch);
     SimDlg->exec();
     delete SimDlg;
 }
